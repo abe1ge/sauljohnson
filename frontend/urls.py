a09b1359ee24738ce42f9622b6cdce865974d82f
fastLittleFoxy <saul.a.johnson@gmail.com>
2014-09-28 17:52:36 +0100

    Flesh out footer, add pages.

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'frontend.views.index'),
-    url(r'contact$', 'frontend.views.contact', name='contact'),
+    url(r'contact$', 'frontend.views.contact'),
+    url(r'linkedin$', 'frontend.views.linkedin'),
+    url(r'privacy$', 'frontend.views.privacy'),
+    url(r'terms$', 'frontend.views.terms'),
+    url(r'login$', 'frontend.views.login'),
 )
--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 
 def index(request):
 	return render_to_response('frontend/index.html', locals())
 
 def contact(request):
 	return render_to_response('frontend/contact.html', locals())
+
+def terms(request):
+	return render_to_response('frontend/terms.html', locals())
+
+def privacy(request):
+	return render_to_response('frontend/privacy.html', locals())
+
+def login(request):
+	return render_to_response('frontend/login.html', locals())	
+
+def linkedin(request):
+	return redirect('https://www.linkedin.com/profile/view?id=323695094')

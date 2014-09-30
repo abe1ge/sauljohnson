bb2c2587a902d93ba76011f2dbc71994a07527dd
fastLittleFoxy <saul.a.johnson@gmail.com>
2014-09-30 21:08:11 +0100

    More pages, more content.

--- frontend/views.py
+++ frontend/views.py
@@ -13,7 +13,24 @@ def privacy(request):
 	return render_to_response('frontend/privacy.html', locals())
 
 def login(request):
-	return render_to_response('frontend/login.html', locals())	
+	return render_to_response('frontend/login.html', locals())
 
 def linkedin(request):
 	return redirect('https://www.linkedin.com/profile/view?id=323695094')
+
+def me_and_the_web(request):
+	return render_to_response('frontend/me-and-the-web.html', locals())
+
+# Projects
+
+def silence(request):
+	return render_to_response('frontend/silence.html', locals())	
+
+def brainfony(request):
+	return render_to_response('frontend/brainfony.html', locals())	
+
+def flatfolio(request):
+	return render_to_response('frontend/flatfolio.html', locals())	
+
+def denobo(request):
+	return render_to_response('frontend/denobo.html', locals())	
